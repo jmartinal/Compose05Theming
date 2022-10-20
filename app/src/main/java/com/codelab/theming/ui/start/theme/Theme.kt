bba69e2aba67e8544b715fdf3806b2e8f5059ab9
Jorge Martin <jorge.martin@pulselive.com>
2022-10-20 12:57:39 +0200

    Added dark theme

--- app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
+++ app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
@@ -1,10 +1,11 @@
-package com.codelab.theming.ui.start
+package com.codelab.theming.ui.start.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import com.codelab.theming.ui.start.theme.*
 
 private val LightColors = lightColors(
     primary = Red700,
@@ -16,10 +17,22 @@ private val LightColors = lightColors(
     error = Red800
 )
 
+private val DarkColors = darkColors(
+    primary = Red300,
+    primaryVariant = Red700,
+    onPrimary = Color.Black,
+    secondary = Red300,
+    onSecondary = Color.Black,
+    error = Red200
+)
+
 @Composable
-fun JetnewsTheme(content: @Composable () -> Unit) {
+fun JetnewsTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
     MaterialTheme(
-        colors = LightColors,
+        colors = if (darkTheme) DarkColors else LightColors,
         typography = JetnewsTypography,
         shapes = JetnewsShapes,
         content = content
